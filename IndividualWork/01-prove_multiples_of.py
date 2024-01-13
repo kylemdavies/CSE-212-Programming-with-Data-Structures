f28c3fec5569b3d54e1e0bebdd6e3837d115c692
kylemdavies <dav19010@byui.edu>
2024-01-13 13:05:26 -0700

    commit

--- IndividualWork/01-prove_multiples_of.py
+++ IndividualWork/01-prove_multiples_of.py
@@ -9,14 +9,7 @@ GitHub repository, unshared Google Drive folder) is acceptable.
 """
 
 def multiples_of(number, length):
-    """
-    This function will produce a list of size 'length' starting
-    with 'number' followed by multiples of 'number'.  For 
-    example, multiples_of(7, 5) will result in:
-    [7, 14, 21, 28, 35].  Assume that length is a positive
-    integer greater than 0.  The implementation must be
-    done using a list comprehension.
-    """
+    my_list = [number * i for i in range(1, length + 1)]
     pass
 
 print(multiples_of(7, 5))    # [7, 14, 21, 28, 35]
