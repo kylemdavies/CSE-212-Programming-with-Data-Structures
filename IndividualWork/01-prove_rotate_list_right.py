ff5c81d820118e19e56bd58579ef513fb0f83e97
kylemdavies <dav19010@byui.edu>
2024-01-13 14:06:51 -0700

    commit

--- IndividualWork/01-prove_rotate_list_right.py
+++ IndividualWork/01-prove_rotate_list_right.py
@@ -9,16 +9,8 @@ GitHub repository, unshared Google Drive folder) is acceptable.
 """
 
 def rotate_list_right(data, amount):
-    """
-    Rotate the 'data' to the right by the 
-    'amount'.   For example, if the data is 
-    [1, 2, 3, 4, 5, 6, 7, 8, 9] and an amount
-    is 5 then the list returned should be 
-    [5, 6, 7, 8, 9, 1, 2, 3, 4].  The value 
-    of amount will be in the range of 1 and 
-    len(data).
-    """
-    result = None  
+    
+    result = [(data[i + amount % len(data)]) for i in range(len(data))]
 
     return result
 
